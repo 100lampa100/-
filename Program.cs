98f3eab03e31ca1de584680e783c3cb576613003
100lampa100 <116907265+100lampa100@users.noreply.github.com>
2022-10-29 20:37:24 +0200

    Add files via upload
    добавил код программы

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+string[] arrInput = new string[5] {"123", "23", "hello", "world", "res"};
+string[] arrTemp = new string[arrInput.Length];
+int count = 0;
+void SecondArray(string[] arrInput, string[] arrTemp)
+{
+    int count = 0;
+    for (int i = 0; i < arrInput.Length; i++)
+    {
+    if(arrInput[i].Length <= 3)
+        {
+        arrTemp[count] = arrInput[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrInput, arrTemp);
+PrintArray(arrTemp)
